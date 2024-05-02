7b8b81f5462b30cdabeb7ac79dbae99ef47fc3cd
William G Underwood <42812654+WGUNDERWOOD@users.noreply.github.com>
2024-05-02 12:24:56 -0400

    Adding .bib test file

--- perf.sh
+++ perf.sh
@@ -1,18 +1,20 @@
 DIR="$(mktemp -d)"
-cp tests/*_in.tex $DIR
+cp tests/*_in.* $DIR
 
 echo "Test files:"
-for f in $DIR/*.tex; do
+for f in $DIR/*; do
     echo -n "  $(basename $f), "
     echo -n "$(wc -l $f | cut --delimiter=" " --fields 1) lines, "
     echo "$(ls -sh $f | cut --delimiter=" " --fields 1)"
 done
-echo -n "Total: $(ls -l $DIR/*.tex | wc -l) files, "
-echo -n "$(wc -l --total=only $DIR/*.tex) lines, "
+
+echo
+echo -n "Total: $(ls -l $DIR/* | wc -l) files, "
+echo -n "$(wc -l --total=only $DIR/*) lines, "
 echo "$(ls -lh $DIR | head -n 1 | cut --delimiter=" " --fields 2)"
 echo
 
 hyperfine --warmup 2 \
-    -n "tex-fmt" "./target/release/tex-fmt $DIR/*.tex" \
-    -n "latexindent" "latexindent $DIR/*.tex" \
-    -n "latexindent -m" "latexindent -m $DIR/*.tex"
+    -n "tex-fmt" "./target/release/tex-fmt $DIR/*" \
+    -n "latexindent" "latexindent $DIR/*" \
+    -n "latexindent -m" "latexindent -m $DIR/*"
