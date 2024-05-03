2b85b67f3c55773814c258307e1930c2447e077c
William G Underwood <42812654+WGUNDERWOOD@users.noreply.github.com>
2024-05-03 18:36:17 -0400

    Adding new file for parsing code

---
+++ src/parse.rs
@@ -0,0 +1,16 @@
+use crate::Parser;
+
+#[derive(Parser)]
+pub struct Cli {
+    #[arg(long, short, help = "Print to stdout, do not modify files")]
+    pub print: bool,
+    #[arg(
+        long,
+        short,
+        help = "Debug mode, disable checks and do not modify files"
+    )]
+    pub debug: bool,
+    #[arg(required = true)]
+    pub filenames: Vec<String>,
+}
+
