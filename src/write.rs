9c7a6a039e8f0d71e23cccc83e3f1fe5eb88e9d6
William G Underwood <42812654+WGUNDERWOOD@users.noreply.github.com>
2024-05-12 16:10:12 -0400

    Removing file backup as not used

--- src/write.rs
+++ src/write.rs
@@ -1,15 +1,5 @@
 use crate::fs;
-use std::env::temp_dir;
 use std::path;
-
-pub fn backup_file(filename: &str) {
-    let filepath = path::Path::new(&filename).canonicalize().unwrap();
-    let mut filebak = temp_dir();
-    filebak.push("tex-fmt");
-    fs::create_dir_all(&filebak).unwrap();
-    filebak.push(filepath.file_name().unwrap());
-    fs::copy(filepath.clone(), &filebak).unwrap();
-}
 
 pub fn write_file(filename: &str, new_file: &str) {
     let filepath = path::Path::new(&filename).canonicalize().unwrap();
