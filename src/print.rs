a92ef60213944ba4f276afd7454739080523f12a
William G Underwood <42812654+WGUNDERWOOD@users.noreply.github.com>
2024-05-18 01:59:50 -0400

    Improve performance by reducing log messages

--- src/print.rs
+++ src/print.rs
@@ -1,11 +1,11 @@
 //use crate::colors::*;
 
 //pub fn print_script_name() {
-    //println!("{}tex-fmt{}", PINK, RESET);
+//println!("{}tex-fmt{}", PINK, RESET);
 //}
 
 //pub fn print_filename(filename: &str) {
-    //println!("{}{}{}", PURPLE, filename, RESET);
+//println!("{}{}{}", PURPLE, filename, RESET);
 //}
 
 pub fn print_file(new_file: &str) {
