38d380106403caa432f8ffe90021fa78514f6002
William G Underwood <42812654+WGUNDERWOOD@users.noreply.github.com>
2024-05-18 21:58:37 -0400

    Combining new line substitutions

--- src/subs.rs
+++ src/subs.rs
@@ -15,11 +15,10 @@ pub fn remove_trailing_spaces(file: &str) -> String {
     RE_TRAIL.replace_all(file, "\n").to_string()
 }
 
-// TODO these should be abstracted together
-pub fn begin_end_environments_new_line(file: &str) -> String {
+pub fn environments_new_line(file: &str) -> String {
     let mut new_file = "".to_string();
     for line in file.lines() {
-        if RE_ENV_BEGIN.is_match(line) || RE_ENV_END.is_match(line) {
+        if RE_ENV_BEGIN.is_match(line) || RE_ENV_END.is_match(line) || RE_ITEM.is_match(line) {
             let comment_index = find_comment_index(line);
             let comment = get_comment(line, comment_index);
             let text = remove_comment(line, comment_index);
@@ -29,23 +28,6 @@ pub fn begin_end_environments_new_line(file: &str) -> String {
             let text = &RE_ENV_END_SHARED_LINE
                 .replace_all(text, "$prev\n$env")
                 .to_string();
-            new_file.push_str(text);
-            new_file.push_str(comment);
-        } else {
-            new_file.push_str(line);
-        }
-        new_file.push('\n');
-    }
-    new_file
-}
-
-pub fn items_new_line(file: &str) -> String {
-    let mut new_file = "".to_string();
-    for line in file.lines() {
-        if RE_ITEM.is_match(line) {
-            let comment_index = find_comment_index(line);
-            let comment = get_comment(line, comment_index);
-            let text = remove_comment(line, comment_index);
             let text = &RE_ITEM_SHARED_LINE
                 .replace_all(text, "$prev\n$env")
                 .to_string();
