e34ec2eb53f5e4f88dd211b71b71ccf4f8f396fb
William G Underwood <42812654+WGUNDERWOOD@users.noreply.github.com>
2024-05-18 22:38:34 -0400

    Implementing some trace level logging

--- src/parse.rs
+++ src/parse.rs
@@ -2,25 +2,35 @@ use clap::Parser;
 
 const EXTENSIONS: [&str; 4] = [".tex", ".bib", ".sty", ".cls"];
 
-#[derive(Parser)]
+#[derive(Debug, Parser)]
 pub struct Cli {
     #[arg(long, short, help = "Indent only, do not modify line breaks")]
     pub indent: bool,
     #[arg(long, short, help = "Print to STDOUT, do not modify files")]
     pub print: bool,
-    #[arg(long, short, help = "Increase verbosity")]
+    #[arg(long, short, help = "Show info log messages")]
     pub verbose: bool,
+    #[arg(long, short, help = "Show trace log messages")]
+    pub trace: bool,
     #[arg(required = true)]
     pub filenames: Vec<String>,
 }
 
 impl Cli {
+
+    pub fn resolve(&mut self) {
+        if self.trace {
+            self.verbose = true;
+        }
+    }
+
     #[cfg(test)]
     pub fn new() -> Self {
         Cli {
             indent: false,
             print: false,
             verbose: false,
+            trace: false,
             filenames: Vec::<String>::new(),
         }
     }
