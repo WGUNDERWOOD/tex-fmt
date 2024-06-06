db6785c6b0470f8d8c8199761199415d58478eaa
William G Underwood <42812654+WGUNDERWOOD@users.noreply.github.com>
2024-06-05 22:16:15 -0400

    alejandra format

--- flake.nix
+++ flake.nix
@@ -4,23 +4,20 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
     flake-utils.url = "github:numtide/flake-utils";
   };
-  outputs =
-    {
-      self,
-      nixpkgs,
-      flake-utils,
-    }:
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
     flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-      in
-      {
+      system: let
+        pkgs = import nixpkgs {inherit system;};
+      in {
         packages = {
-          default = pkgs.callPackage ./default.nix { inherit pkgs; };
+          default = pkgs.callPackage ./default.nix {inherit pkgs;};
         };
         devShells = {
-          default = pkgs.callPackage ./shell.nix { inherit pkgs; };
+          default = pkgs.callPackage ./shell.nix {inherit pkgs;};
         };
       }
     );
