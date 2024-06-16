a4be7680e3d99c91238b885e51119defc0a9db05
William G Underwood <42812654+WGUNDERWOOD@users.noreply.github.com>
2024-06-16 12:07:37 +0100

    Fix failing latex test

--- extra/latex.sh
+++ extra/latex.sh
@@ -2,13 +2,6 @@ echo "Checking latex PDFs agree"
 DIR="$(mktemp -d)"
 cp -r ../tests/* $DIR
 echo $DIR
-
-# TODO remove block
-rm $DIR/in/*
-cp -r ../tests/in/higher* $DIR/in/
-cp -r ../tests/in/cam* $DIR/in/
-cp -r ../tests/in/quiver* $DIR/in/
-
 cd $DIR
 
 echo
@@ -16,8 +9,8 @@ echo
 for file in ./in/*.tex; do
     f=$(basename $file .tex)
     echo "Running latex for $f.tex"
-    (cd ./in && latexmk -pdflua $f.tex )#>/dev/null 2>&1)
-    (cd ./out && latexmk -pdflua $f.tex )#>/dev/null 2>&1)
+    (cd ./in && latexmk -pdflua $f.tex >/dev/null 2>&1)
+    (cd ./out && latexmk -pdflua $f.tex >/dev/null 2>&1)
     (cd ./in && pdftotext -q $f.pdf >/dev/null 2>&1)
     (cd ./out && pdftotext -q $f.pdf >/dev/null 2>&1)
 done
