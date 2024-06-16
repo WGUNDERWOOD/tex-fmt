e454e9abbbb85a5c7aab9b7aefca827a33d560f2
William G Underwood <42812654+WGUNDERWOOD@users.noreply.github.com>
2024-06-16 17:24:50 +0100

    Better names for files and file contents

--- src/write.rs
+++ src/write.rs
@@ -1,7 +1,7 @@
 use crate::fs;
 use std::path;
 
-pub fn write_file(filename: &str, new_file: &str) {
-    let filepath = path::Path::new(&filename).canonicalize().unwrap();
-    fs::write(filepath, new_file).expect("Could not write the new file");
+pub fn write_file(file: &str, text: &str) {
+    let filepath = path::Path::new(&file).canonicalize().unwrap();
+    fs::write(filepath, text).expect("Could not write the file");
 }
