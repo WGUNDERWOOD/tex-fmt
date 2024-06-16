fbe1a954677670bf7a7b7e0824073c4fee43adf8
William G Underwood <42812654+WGUNDERWOOD@users.noreply.github.com>
2024-06-16 17:15:08 +0100

    Shellcheck tests passing

--- extra/latex.sh
+++ extra/latex.sh
@@ -1,26 +1,27 @@
+#!/usr/bin/env bash
 echo "Checking latex PDFs agree"
 DIR="$(mktemp -d)"
-cp -r ../tests/* $DIR
-echo $DIR
-cd $DIR
+cp -r ../tests/* "$DIR"
+echo "$DIR"
+cd "$DIR" || exit
 
 echo
 
 for file in ./source/*.tex; do
-    f=$(basename $file .tex)
+    f=$(basename "$file" .tex)
     echo "Running latex for $f.tex"
-    (cd ./source && latexmk -pdflua $f.tex >/dev/null 2>&1)
-    (cd ./target && latexmk -pdflua $f.tex >/dev/null 2>&1)
-    (cd ./source && pdftotext -q $f.pdf >/dev/null 2>&1)
-    (cd ./target && pdftotext -q $f.pdf >/dev/null 2>&1)
+    (cd ./source && latexmk -pdflua "$f.tex" >/dev/null 2>&1)
+    (cd ./target && latexmk -pdflua "$f.tex" >/dev/null 2>&1)
+    (cd ./source && pdftotext -q "$f.pdf" >/dev/null 2>&1)
+    (cd ./target && pdftotext -q "$f.pdf" >/dev/null 2>&1)
 done
 
 echo
 
 for file in ./source/*.tex; do
-    f=$(basename $file .tex)
+    f=$(basename "$file" .tex)
     echo "Checking PDF for $f.tex"
-    diff -u ./source/$f.txt ./target/$f.txt | diff-so-fancy
+    diff -u "source/$f.txt" "target/$f.txt" | diff-so-fancy
 done
 
-echo $DIR
+echo "$DIR"
--- extra/prof.sh
+++ extra/prof.sh
@@ -1,22 +1,23 @@
+#!/usr/bin/env bash
 echo "Making flamegraph profile"
 DIR="$(mktemp -d)"
-cp ../tests/in/phd_dissertation.tex $DIR
+cp "../tests/source/phd_dissertation.tex" "$DIR"
 cargo build --release
 
 echo "Writing large test file"
-for i in {1..5}; do
-    cat $DIR/phd_dissertation.tex >> $DIR/large.tex
-    echo "\n\n\n" >> $DIR/large.tex
+for _ in {1..5}; do
+    cat "$DIR/phd_dissertation.tex" >> "$DIR/large.tex"
+    printf "\n\n\n" >> "$DIR/large.tex"
 done
 
-rm $DIR/phd_dissertation.tex
+rm "$DIR/phd_dissertation.tex"
 
 echo "Test file:"
-for f in $DIR/*.tex; do
-    echo -n "  $(basename $f), "
-    echo -n "$(wc -l $f | cut --delimiter=" " --fields 1) lines, "
-    echo "$(ls -sh $f | cut --delimiter=" " --fields 1)"
+for f in "$DIR"/*.tex; do
+    echo -n "  $(basename "$f"), "
+    echo -n "$(wc -l "$f" | cut --delimiter=" " --fields 1) lines, "
+    du -h "$f" | cut --fields 1
 done
 
-flamegraph -F 10000 -- ../target/release/tex-fmt $DIR/*.tex
+flamegraph -F 10000 -- ../target/release/tex-fmt "$DIR"/*.tex
 hyperfine --warmup 2 -n "tex-fmt" "../target/release/tex-fmt $DIR/*.tex"
