d6de6aaa5e20b06e6b077a09d044d79a1d952e0b
William G Underwood <42812654+WGUNDERWOOD@users.noreply.github.com>
2024-07-30 21:21:57 +0100

    Working on logo

--- shell.nix
+++ shell.nix
@@ -1,15 +1,21 @@
 {pkgs ? import <nixpkgs> {}}:
 pkgs.mkShell {
   inputsFrom = [(pkgs.callPackage ./default.nix {})];
-  buildInputs = with pkgs; [
-    rustfmt
-    clippy
-    cargo-flamegraph
-    cacert
-    hyperfine
-    texlive.combined.scheme-full
-    diff-so-fancy
-    poppler_utils
-    shellcheck
+  buildInputs = let
+    python = pkgs.python3.withPackages (ps:
+      with ps; [
+        matplotlib
+      ]);
+  in [
+    pkgs.cacert
+    pkgs.cargo-flamegraph
+    pkgs.clippy
+    pkgs.diff-so-fancy
+    pkgs.hyperfine
+    pkgs.poppler_utils
+    python
+    pkgs.rustfmt
+    pkgs.shellcheck
+    pkgs.texlive.combined.scheme-full
   ];
 }
