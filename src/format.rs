9ecc47844275320207b483a29e6555a4c96acda3
William G Underwood <42812654+WGUNDERWOOD@users.noreply.github.com>
2024-08-01 22:46:31 +0100

    started work on onepass refactor

--- src/format.rs
+++ src/format.rs
@@ -7,7 +7,7 @@ use log::Level::{Info, Warn};
 
 const MAX_PASS: usize = 10;
 
-fn apply_passes(
+fn apply_indent_wrap(
     text: &str,
     file: &str,
     args: &Cli,
@@ -54,6 +54,55 @@ fn apply_passes(
     new_text
 }
 
+/*
+fn apply_passes(
+    text: &str,
+    file: &str,
+    args: &Cli,
+    logs: &mut Vec<Log>,
+) -> String {
+    let mut new_text = apply_indent(text, file, args, logs, Some(1));
+    let mut finished = false;
+    let mut pass = 2;
+
+    while !finished && needs_wrap(&new_text) && pass < MAX_PASS + 2 {
+        let old_text = new_text.clone();
+        new_text = wrap(&new_text, file, logs, Some(pass), args);
+        new_text = remove_trailing_spaces(&new_text);
+        new_text = apply_indent(&new_text, file, args, logs, Some(pass));
+        pass += 1;
+        if new_text == old_text {
+            finished = true;
+        }
+    }
+
+    record_log(
+        logs,
+        Info,
+        None,
+        file.to_string(),
+        None,
+        None,
+        "Passes completed.".to_string(),
+    );
+
+    // check indents return to zero
+    if new_text.lines().last().unwrap_or_default().starts_with(' ') {
+        record_log(
+            logs,
+            Warn,
+            None,
+            file.to_string(),
+            None,
+            None,
+            "Indent does not return to zero.".to_string(),
+        );
+    }
+
+    new_text
+}
+*/
+
 pub fn format_file(
     text: &str,
     file: &str,
@@ -75,6 +124,6 @@ pub fn format_file(
     new_text = environments_new_line(&new_text, file, args, logs);
     new_text = remove_tabs(&new_text);
     new_text = remove_trailing_spaces(&new_text);
-    new_text = apply_passes(&new_text, file, args, logs);
+    new_text = apply_indent_wrap(&new_text, file, args, logs);
     new_text
 }
