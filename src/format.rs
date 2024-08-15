294d0f47adc5d147b5d4c7123281bec2fc38a47a
William G Underwood <42812654+WGUNDERWOOD@users.noreply.github.com>
2024-08-15 21:16:51 +0100

    New structs

--- src/format.rs
+++ src/format.rs
@@ -7,49 +7,19 @@ use log::Level::{Info, Warn};
 
 const MAX_PASS: usize = 10;
 
+struct Format {
+    linum: usize,
+    ignore: Ignore,
+    indent: Indent,
+    leave: Leave,
+}
+
 fn apply_indent_wrap(
     text: &str,
     file: &str,
     args: &Cli,
     logs: &mut Vec<Log>,
 ) -> String {
-    let mut new_text = apply_indent(text, file, args, logs, Some(1));
-    let mut finished = false;
-    let mut pass = 2;
-
-    while !finished && needs_wrap(&new_text) && pass < MAX_PASS + 2 {
-        let old_text = new_text.clone();
-        new_text = wrap(&new_text, file, logs, Some(pass), args);
-        new_text = remove_trailing_spaces(&new_text);
-        new_text = apply_indent(&new_text, file, args, logs, Some(pass));
-        pass += 1;
-        if new_text == old_text {
-            finished = true;
-        }
-    }
-
-    record_log(
-        logs,
-        Info,
-        None,
-        file.to_string(),
-        None,
-        None,
-        "Passes completed.".to_string(),
-    );
-
-    // check indents return to zero
-    if new_text.lines().last().unwrap_or_default().starts_with(' ') {
-        record_log(
-            logs,
-            Warn,
-            None,
-            file.to_string(),
-            None,
-            None,
-            "Indent does not return to zero.".to_string(),
-        );
-    }
 
     new_text
 }
