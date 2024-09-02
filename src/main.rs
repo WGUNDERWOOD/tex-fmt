972abe10498f55ef4601c5468f0752a4943cd723
William G Underwood <42812654+WGUNDERWOOD@users.noreply.github.com>
2024-09-02 10:15:53 +0100

    Make clippy more pedantic

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+#![warn(missing_docs)]
+#![warn(clippy::nursery)]
+#![warn(clippy::cargo)]
+#![warn(clippy::missing_docs_in_private_items)]
+#![warn(clippy::pedantic)]
+#![allow(clippy::wildcard_imports)]
+#![allow(clippy::module_name_repetitions)]
+
 use clap::Parser;
 use log::Level::Error;
 use std::fs;
