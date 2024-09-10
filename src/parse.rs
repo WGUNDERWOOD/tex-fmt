ea13a29822cf7eb1f74b814b524220cd971eb2d8
León Bohn <bohn@lics.rwth-aachen.de>
2024-09-10 17:22:36 +0200

    Add --stdio to process STDIN

--- src/parse.rs
+++ src/parse.rs
@@ -4,6 +4,7 @@ use crate::logging::*;
 use crate::regexes::*;
 use clap::Parser;
 use log::Level::Error;
+use log::LevelFilter;
 use std::fs;
 
 /// Command line arguments
@@ -24,15 +25,46 @@ pub struct Cli {
     pub quiet: bool,
     #[arg(long, short, help = "Show trace log messages")]
     pub trace: bool,
-    #[arg(required = true)]
+    #[arg(help = "List of files that should be formatted")]
     pub files: Vec<String>,
+    #[arg(
+        long,
+        short,
+        help = "Process STDIN as a single file and output formatted text to STDOUT"
+    )]
+    pub stdin: bool,
 }
 
 impl Cli {
+    /// Get the log level.
+    pub const fn log_level(&self) -> LevelFilter {
+        if self.trace {
+            LevelFilter::Trace
+        } else if self.verbose {
+            LevelFilter::Info
+        } else if self.quiet {
+            LevelFilter::Error
+        } else {
+            LevelFilter::Warn
+        }
+    }
+
     /// Ensure the provided arguments are consistent
     pub fn resolve(&mut self) {
+        use std::process::exit;
+
         if self.trace {
             self.verbose = true;
+        }
+        self.print |= self.stdin;
+
+        if !self.stdin && self.files.is_empty() {
+            log::error!("No files specified, either provide at least one filename or set the --stdin flag.");
+            exit(1);
+        }
+        if self.stdin && !self.files.is_empty() {
+            log::warn!("Provided file name(s) will be ignored when using the --stdin flag.");
+            self.files.clear();
         }
     }
 
@@ -43,6 +75,7 @@ impl Cli {
             print: false,
             keep: false,
             verbose: false,
+            stdin: false,
             quiet: false,
             trace: false,
             files: Vec::<String>::new(),
@@ -68,4 +101,31 @@ pub fn read(file: &str, logs: &mut Vec<Log>) -> Option<(String, String)> {
         record_file_log(logs, Error, file, "File type invalid.");
     }
     None
+}
+
+/// Attempts to read from STDIN and return the filename `<STDIN>` and text.
+pub fn read_stdin(logs: &mut Vec<Log>) -> Option<(String, String)> {
+    use std::io::Read;
+
+    let mut text = String::new();
+    match std::io::stdin().read_to_string(&mut text) {
+        Ok(bytes) => {
+            record_file_log(
+                logs,
+                log::Level::Trace,
+                "<STDIN>",
+                &format!("Read {bytes} bytes."),
+            );
+            Some((String::from("<STDIN>"), text))
+        }
+        Err(e) => {
+            record_file_log(
+                logs,
+                Error,
+                "<STDIN>",
+                &format!("Could not read from STDIN: {e}"),
+            );
+            None
+        }
+    }
 }
