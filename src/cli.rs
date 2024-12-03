01ffd9dfb862bec1b5edb3efe1b43d449e93a7b0
William G Underwood <42812654+WGUNDERWOOD@users.noreply.github.com>
2024-12-03 09:35:16 +0000

    Compile-time completions

--- src/cli.rs
+++ src/cli.rs
@@ -1,107 +1,10 @@
 //! Functionality to parse CLI arguments
 
 use crate::args::*;
-use clap::{command, value_parser, Arg, ArgAction, ArgMatches, Command, ValueHint};
-use clap_complete::{generate, Generator, Shell};
 use log::LevelFilter;
-use std::borrow::ToOwned;
-use std::path::PathBuf;
-use std::io;
-use ArgAction::{Append, SetTrue};
 
-/// Construct the CLI command
-fn get_cli_command() -> Command {
-    let command = command!()
-        .arg(
-            Arg::new("check")
-                .short('c')
-                .long("check")
-                .action(SetTrue)
-                .help("Check formatting, do not modify files"),
-        )
-        .arg(
-            Arg::new("print")
-                .short('p')
-                .long("print")
-                .action(SetTrue)
-                .help("Print to stdout, do not modify files"),
-        )
-        .arg(
-            Arg::new("nowrap")
-                .short('n')
-                .long("nowrap")
-                .action(SetTrue)
-                .help("Do not wrap long lines"),
-        )
-        .arg(
-            Arg::new("verbose")
-                .short('v')
-                .long("verbose")
-                .action(SetTrue)
-                .help("Show info messages"),
-        )
-        .arg(
-            Arg::new("quiet")
-                .short('q')
-                .long("quiet")
-                .action(SetTrue)
-                .help("Hide warning messages"),
-        )
-        .arg(
-            Arg::new("trace")
-                .long("trace")
-                .action(SetTrue)
-                .help("Show trace messages"),
-        )
-        .arg(
-            Arg::new("files")
-                .action(Append)
-                .help("List of files to be formatted"),
-        )
-        .arg(
-            Arg::new("stdin")
-                .short('s')
-                .long("stdin")
-                .action(SetTrue)
-                .help("Process stdin as a single file, output to stdout"),
-        )
-        .arg(
-            Arg::new("tabsize")
-                .short('t')
-                .long("tabsize")
-                .value_parser(value_parser!(u8))
-                .help("Number of characters to use as tab size [default: 2]"),
-        )
-        .arg(
-            Arg::new("usetabs")
-                .long("usetabs")
-                .action(SetTrue)
-                .help("Use tabs instead of spaces for indentation"),
-        )
-        .arg(
-            Arg::new("wraplen")
-                .short('l')
-                .long("wraplen")
-                .value_parser(value_parser!(u8))
-                .help("Line length for wrapping [default: 80]"),
-        )
-        .arg(
-            Arg::new("config")
-                .long("config")
-                .help("Configuration file path")
-                .value_parser(value_parser!(PathBuf))
-                .value_hint(ValueHint::FilePath)
-                .help("Path to config file"),
-        );
-
-    Command::new("completion")
-        .arg(
-            Arg::new("generator")
-            .long("generate")
-            .value_parser(value_parser!(Shell)),
-            )
-        .subcommand(command)
-}
+// Get the clap CLI command from a separate file
+include!("command.rs");
 
 /// Parse CLI arguments into `OptionArgs` struct
 pub fn get_cli_args() -> OptionArgs {
