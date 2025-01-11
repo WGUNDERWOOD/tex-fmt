de8c912f79b2765dfb85272f521136c3cd027f73
William G Underwood <42812654+WGUNDERWOOD@users.noreply.github.com>
2025-01-11 14:05:09 +0000

    HTML update

--- src/wasm.rs
+++ src/wasm.rs
@@ -6,20 +6,14 @@ use crate::logging::*;
 
 #[wasm_bindgen]
 pub fn main(text: &str) -> String {
+    // TODO Display logs on web page
+    // TODO Read user-supplied config file
     let mut args = Args {
         stdin: true,
         ..Default::default()
     };
-    //init_logger(args.verbosity);
     let mut logs = Vec::<Log>::new();
     args.resolve(&mut logs);
     let file = "input";
-    record_file_log(&mut logs, log::Level::Error, "", "");
-    let new_text = format_file(text, file, &args, &mut logs);
-    new_text
-}
-
-#[wasm_bindgen]
-extern "C" {
-    pub fn alert(s: &str);
+    format_file(text, file, &args, &mut logs)
 }
