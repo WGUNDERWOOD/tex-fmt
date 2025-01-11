ddb5de797d5516171438bfebcd9e5170610c5475
William G Underwood <42812654+WGUNDERWOOD@users.noreply.github.com>
2025-01-11 14:57:16 +0000

    Show logging on web page

--- src/wasm.rs
+++ src/wasm.rs
@@ -1,3 +1,5 @@
+use js_sys::{Object, Reflect};
+use log::LevelFilter;
 use wasm_bindgen::prelude::*;
 
 use crate::args::*;
@@ -5,15 +7,24 @@ use crate::format::*;
 use crate::logging::*;
 
 #[wasm_bindgen]
-pub fn main(text: &str) -> String {
-    // TODO Display logs on web page
-    // TODO Read user-supplied config file
+pub fn main(text: &str) -> JsValue {
+    // Set up arguments
     let mut args = Args {
         stdin: true,
+        verbosity: LevelFilter::Warn,
         ..Default::default()
     };
+
+    // Run tex-fmt
     let mut logs = Vec::<Log>::new();
     args.resolve(&mut logs);
     let file = "input";
-    format_file(text, file, &args, &mut logs)
+    let new_text = format_file(text, file, &args, &mut logs);
+    let logs = format_logs(&mut logs, &args);
+
+    // Wrap into JS object
+    let js_object = Object::new();
+    Reflect::set(&js_object, &"output".into(), &new_text.into()).unwrap();
+    Reflect::set(&js_object, &"logs".into(), &logs.into()).unwrap();
+    js_object.into()
 }
