7776e3fd7005f20a8a16045757cecfb89463d3ff
William G Underwood <42812654+WGUNDERWOOD@users.noreply.github.com>
2025-01-13 11:08:48 +0000

    Fix prof script

--- extra/binary.sh
+++ extra/binary.sh
@@ -5,12 +5,12 @@ cp -r ../tests/* "$DIR"
 cargo build --release
 
 # run tex-fmt
-for TESTDIR in $DIR/*; do
+for TESTDIR in "$DIR"/*; do
     ../target/release/tex-fmt -q "$TESTDIR/source"/*
 done
 
-# tex-fmt agrees with target files
-for TESTDIR in $DIR/*; do
+# check tex-fmt agrees with target files
+for TESTDIR in "$DIR"/*; do
     for file in "$TESTDIR/source"/*; do
         f=$(basename "$file")
         diff "$TESTDIR/target/$f" "$TESTDIR/source/$f" | diff-so-fancy
