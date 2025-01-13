4ecf966e9df4e1fffa617c3a0c070d490d1d6b5d
William G Underwood <42812654+WGUNDERWOOD@users.noreply.github.com>
2025-01-13 11:08:48 +0000

    Fix prof script

--- extra/latex.sh
+++ extra/latex.sh
@@ -5,23 +5,30 @@ cp -r ../tests/* "$DIR"
 echo "$DIR"
 cd "$DIR" || exit
 
+# empty file cannot be compiled
+rm -r ./empty/
+
 echo
 
-for file in ./source/*.tex; do
-    f=$(basename "$file" .tex)
-    echo "Running latex for $f.tex"
-    (cd ./source && latexmk -pdflua "$f.tex")
-    (cd ./target && latexmk -pdflua "$f.tex")
-    (cd ./source && pdftotext -q "$f.pdf")
-    (cd ./target && pdftotext -q "$f.pdf")
+for TESTDIR in "$DIR"/*; do
+    for file in "$TESTDIR/source"/*.tex; do
+        f=$(basename "$file" .tex)
+        echo "Running latex for $f.tex"
+        (cd "$TESTDIR/source" && latexmk -pdflua "$f.tex")
+        (cd "$TESTDIR/target" && latexmk -pdflua "$f.tex")
+        (cd "$TESTDIR/source" && pdftotext -q "$f.pdf")
+        (cd "$TESTDIR/target" && pdftotext -q "$f.pdf")
+    done
 done
 
 echo
 
-for file in ./source/*.tex; do
-    f=$(basename "$file" .tex)
-    echo "Checking PDF for $f.tex"
-    diff -u "source/$f.txt" "target/$f.txt" | diff-so-fancy
+for TESTDIR in "$DIR"/*; do
+    for file in "$TESTDIR/source"/*.tex; do
+        f=$(basename "$file" .tex)
+        echo "Checking PDF for $f.tex"
+        diff -u "$TESTDIR/source/$f.txt" "$TESTDIR/target/$f.txt" | diff-so-fancy
+    done
 done
 
 echo "$DIR"
