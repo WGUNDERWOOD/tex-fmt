1aa7c664e2ae1e441a2abf6aa485c2f0eb08c889
William G Underwood <42812654+WGUNDERWOOD@users.noreply.github.com>
2025-01-13 11:51:55 +0000

    Config files for new tests

--- src/tests.rs
+++ src/tests.rs
@@ -1,13 +1,29 @@
 use crate::args::*;
+use crate::config::*;
 use crate::format::format_file;
 use crate::logging::*;
 use colored::Colorize;
+use merge::Merge;
 use similar::{ChangeTag, TextDiff};
 use std::fs;
 use std::path::PathBuf;
 
-fn test_file(source_file: &str, target_file: &str) -> bool {
-    let args = Args::default();
+fn test_file(
+    source_file: &str,
+    target_file: &str,
+    config_file: Option<PathBuf>,
+) -> bool {
+    // Get arguments from config file if it exists
+    let mut args = OptionArgs::new();
+    args.config = config_file;
+    let config = get_config(&args);
+    let config_args = get_config_args(config);
+    if let Some(c) = config_args {
+        args.merge(c);
+    }
+    args.merge(OptionArgs::default());
+    let args = Args::from(args);
+
     let mut logs = Vec::<Log>::new();
     let source_text = fs::read_to_string(source_file).unwrap();
     let target_text = fs::read_to_string(target_file).unwrap();
@@ -63,9 +79,19 @@ fn test_source() {
         for file in source_files {
             let in_file = test_dir.path().join("source").join(file.clone());
             let out_file = test_dir.path().join("target").join(file.clone());
+            let config_file = test_dir.path().join("tex-fmt.toml");
+            let config_file = if config_file.exists() {
+                Some(config_file)
+            } else {
+                None
+            };
             dbg!(&in_file);
-            if !test_file(in_file.to_str().unwrap(), out_file.to_str().unwrap())
-            {
+            dbg!(&config_file);
+            if !test_file(
+                in_file.to_str().unwrap(),
+                out_file.to_str().unwrap(),
+                config_file,
+            ) {
                 panic!("Failed in {file}");
             }
         }
@@ -81,9 +107,17 @@ fn test_target() {
         let target_files = read_files_from_dir(&target_dir);
         for file in target_files {
             let in_file = test_dir.path().join("target").join(file.clone());
-            dbg!(&in_file);
-            if !test_file(in_file.to_str().unwrap(), in_file.to_str().unwrap())
-            {
+            let config_file = test_dir.path().join("tex-fmt.toml");
+            let config_file = if config_file.exists() {
+                Some(config_file)
+            } else {
+                None
+            };
+            if !test_file(
+                in_file.to_str().unwrap(),
+                in_file.to_str().unwrap(),
+                config_file,
+            ) {
                 panic!("Failed in {file}");
             }
         }
@@ -102,12 +136,14 @@ fn test_short() {
         if !test_file(
             &format!("tests/{source_file}"),
             &format!("tests/{target_file}"),
+            None,
         ) {
             fail = true;
         }
         if !test_file(
             &format!("tests/{target_file}"),
             &format!("tests/{target_file}"),
+            None,
         ) {
             fail = true;
         }
