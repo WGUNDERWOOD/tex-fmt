dc3145a25ab38869646e06285f9523c3ae90b94c
William G Underwood <42812654+WGUNDERWOOD@users.noreply.github.com>
2025-05-05 18:16:30 +0100

    Tests passing and simpler wrapping logic

--- src/tests.rs
+++ src/tests.rs
@@ -208,7 +208,12 @@ fn test_all() {
 #[test]
 #[ignore]
 fn test_subset() {
-    let test_names = ["wrap_chars"];
+    let test_names = [
+        //"wrap_chars",
+        //"cv",
+        //"short_document",
+        "wrap",
+    ];
     let mut pass = true;
     let test_dirs = fs::read_dir("./tests/").unwrap().filter(|d| {
         test_names.iter().any(|t| {
