4aa7c1528be8eb25567db956da540443ddf4142b
Brian C. Ferrari <48806333+Cavenfish@users.noreply.github.com>
2025-06-29 08:48:51 +0200

    use reference in find files

--- src/search.rs
+++ src/search.rs
@@ -7,7 +7,7 @@ use std::path::PathBuf;
 /// # Panics
 ///
 /// This function panics when a file name cannot be converted to a string.
-pub fn find_files(dir: PathBuf, files: &mut Vec<PathBuf>) {
+pub fn find_files(dir: &PathBuf, files: &mut Vec<PathBuf>) {
     // Recursive walk of passed directory (ignore errors, symlinks and non-dirs)
     if dir.is_dir() {
         for entry in Walk::new(dir).filter_map(std::result::Result::ok) {
