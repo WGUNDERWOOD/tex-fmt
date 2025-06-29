e1d2ebea6f0cfa240f77946b495f53313f31db36
Brian C. Ferrari <48806333+Cavenfish@users.noreply.github.com>
2025-06-29 09:01:09 +0200

    move dir check out of find files

--- src/search.rs
+++ src/search.rs
@@ -9,14 +9,12 @@ use std::path::PathBuf;
 /// This function panics when a file name cannot be converted to a string.
 pub fn find_files(dir: &PathBuf, files: &mut Vec<PathBuf>) {
     // Recursive walk of passed directory (ignore errors, symlinks and non-dirs)
-    if dir.is_dir() {
-        for entry in Walk::new(dir).filter_map(std::result::Result::ok) {
-            // If entry is file and has accepted extension, push to files
-            if entry.file_type().unwrap().is_file() {
-                let file = entry.path();
-                if EXTENSIONS.iter().any(|e| file.ends_with(e)) {
-                    files.push(file.to_path_buf());
-                }
+    for entry in Walk::new(dir).filter_map(std::result::Result::ok) {
+        // If entry is file and has accepted extension, push to files
+        if entry.file_type().unwrap().is_file() {
+            let file = entry.path();
+            if EXTENSIONS.iter().any(|e| file.ends_with(e)) {
+                files.push(file.to_path_buf());
             }
         }
     }
